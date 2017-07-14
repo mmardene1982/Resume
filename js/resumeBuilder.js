2e94cea3a8eb28c8a1e0eafe07117ff5074349f7
maher mardini <mmardene1982@gmail.com>
2017-07-14 17:55:19 +0200

    modified to meet the requirment

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,11 +5,12 @@ var bio = {
 	"name" : "Maher Mardini",
 	"role" : "Front-End Developer",
 	"welcomeMessage" : "I have been always amazed in computer science, there for after finishing my BSc in electrical engineering, I have studied E-business and computing and now I am finishing my Nanodegree in Front-end web developing.y technical expertise includes HTML, CSS, and Java script and willing to continue learning other languages as well. Also I have excellent expertise in (Mac, Windows, and Linux).", 
-	"image" : "images/profile.png",
+	"biopic" : "images/profile.png",
 	"contacts" : [{
 		"mobile" : "+47 957 24 036",
 		"location" : "Norway",
-		"email" : "mmardene1982@gmail.com"
+		"email" : "mmardene1982@gmail.com",
+		"github" : "https://github.com/mmardene1982"
 	}],
 	"skills" : ["HTML" , "CSS" , "JavaScript" , "Mac" , "Windows" , "Linux-Ubuntu"]
 
@@ -48,7 +49,7 @@ var work = {
 };
 
 var projects = {
-	"project" : [{
+	"projects" : [{
 		"title" : "Mocup to portfolio ",
 		"dates" : "2017", 
 		"description" : "Using the knowledge that I learned throughout the course I built and portfolio describing the latest best movies for 2017 including dome JS. ", 
@@ -68,7 +69,7 @@ var education = {
 		"name" : "Edexcel International University",
 		"location" : "United Kingdom",
 		"degree" : "Diploma", 
-		"majors" : "HND in E-business and computing",
+		"majors" : ["HND in E-business and computing"],
 		"dates" : "2008 – 2010",
 		"url" : "https://www.edexcelonline.com"
 	},
@@ -76,7 +77,7 @@ var education = {
 		"name" : "Damascus University",
 		"location" : "Damascus, Syria",
 		"degree" : "Bachelor Degree", 
-		"majors" : "Electrical Engineering",
+		"majors" : ["Electrical Engineering"],
 		"dates" : "2001 – 2005",
 		"url" : "http://damasuniv.edu.sy"
 	}],
@@ -91,7 +92,7 @@ var education = {
 bio.display = function(){
 	var formattedName = HTMLheaderName.replace("%data%",bio.name);
 	var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-	var formattedImage = HTMLbioPic.replace("%data%",bio.image);
+	var formattedImage = HTMLbioPic.replace("%data%",bio.biopic);
 	var formattedMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
 
 	$("#header").prepend(formattedRole);
@@ -107,15 +108,17 @@ bio.display = function(){
 	for(var contact in bio.contacts) {
 		var formattedMobile = HTMLmobile.replace("%data%",bio.contacts[contact].mobile);
 		var formattedEmail = HTMLemail.replace("%data%",bio.contacts[contact].email);
+		var formattedGithub = HTMLgithub.replace("%data%",bio.contacts[contact].github);
 		var formattedLocation = HTMLlocation.replace("%data%",bio.contacts[contact].location);
 		$("#footerContacts").append(formattedMobile);
 		$("#footerContacts").append(formattedEmail);
+		$("#footerContacts").append(formattedGithub);
 		$("#footerContacts").append(formattedLocation);
 	}
 };
 bio.display();
 
-function displayWork() {
+work.display = function(){
 	for (var job in work.jobs){
 	$("#workExperience").append(HTMLworkStart);
 	var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
@@ -130,24 +133,24 @@ function displayWork() {
 	$(".work-entry:last").append(formattedDescription);
 }
 }
-displayWork();
+work.display()
 
-function displayProjects(){
-	for (var project in projects.project){
+projects.display = function(){
+	for (var project in projects.projects){
 		$("#projects").append(HTMLprojectStart);
-		var formatedProjectTitle = HTMLprojectTitle.replace("%data%",projects.project[project].title);
+		var formatedProjectTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
 		$(".project-entry:last").append(formatedProjectTitle);
-		var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.project[project].dates);
+		var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
 		$(".project-entry:last").append(formattedProjectDates);
-		var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.project[project].description);
+		var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description);
 		$(".project-entry:last").append(formattedProjectDescription);
-		for(var img in projects.project[project].images){
-			var formattedImage = HTMLprojectImage.replace("%data%",projects.project[project].images[img]);
+		for(var img in projects.projects[project].images){
+			var formattedImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[img]);
 			$(".project-entry:last").append(formattedImage);
 		}
 	}
 }
-displayProjects();
+projects.display()
 
 function displayEdu(){
 	for(var item in education.schools){
